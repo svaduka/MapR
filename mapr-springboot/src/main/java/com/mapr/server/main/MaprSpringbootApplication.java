f29ff7475ff5dd2087743363e00ebf04d279726a
svaduka <sainagaraju.vaduka@gmail.com>
2019-01-27 15:27:01 +0530

    Created Sprint Boot Application for MapR-DB

---
+++ mapr-springboot/src/main/java/com/mapr/server/main/MaprSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.mapr.server.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaprSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaprSpringbootApplication.class, args);
+	}
+
+}
+
