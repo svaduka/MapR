6d6a591405cbc8b642c78cb124760309d761489b
svaduka <sainagaraju.vaduka@gmail.com>
2019-01-28 17:57:42 +0530

    Created MapRDB poc project seperate

---
+++ maprdb-poc/src/main/java/mapr/com/maprdb/util/MapRDBUtil.java
@@ -0,0 +1,59 @@
+package mapr.com.maprdb.util;
+
+import java.util.Random;
+
+import org.ojai.Document;
+import org.ojai.store.Connection;
+import org.ojai.store.DocumentStore;
+import org.ojai.store.DriverManager;
+
+public class MapRDBUtil {
+
+	private static Connection connection = null;
+	static {
+		connection = DriverManager.getConnection("ojai:mapr:");
+	}
+	
+	public static boolean writeMsgToMapRDB(final String msg, final String tableName) {
+
+		// Create Document from json Msg
+
+		if (connection == null) {
+			connection = DriverManager.getConnection("ojai:mapr:");
+		}
+
+		DocumentStore store = connection.getStore(tableName);
+
+		Document userDocument = connection.newDocument(msg);
+		userDocument.setId(Integer.toString(
+				userDocument.getString("__Id") == null ? new Random().nextInt(100000) : userDocument.getInt("__Id")));
+		store.insertOrReplace(userDocument);
+		store.close();
+		return Boolean.TRUE;
+	}
+	
+	public static String readJsonFromID(final String id,final String tableName) 
+	{
+		if (connection == null) {
+			connection = DriverManager.getConnection("ojai:mapr:");
+		}
+		
+		String data = null;
+
+		System.out.println("Id:"+id);
+	    // Get an instance of OJAI DocumentStore
+	    final DocumentStore store = connection.getStore(tableName);
+
+	    // fetch the OJAI Document by its '_id' field
+	    final Document document = store.findById(id);
+
+	    // Print the OJAI Document
+	    data=document.asJsonString();      
+	    // Close this instance of OJAI DocumentStore
+	    store.close();
+	    System.out.println("data:"+data);
+		
+		return data;
+	}
+	
+}
